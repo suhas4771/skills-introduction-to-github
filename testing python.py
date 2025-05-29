25104ff88d6eaae6be31b10763d25773e1e87dc0
suhas4771 <suhas.bandari5@gmail.com>
2025-05-29 14:58:44 +0530

    this is still in testing

---
+++ testing python.py
@@ -0,0 +1,8 @@
+print("hello world ")
+x=input("what is your name???")
+if x== "suhas" :
+    print("hello admin ")
+elif x!= "suhas" :
+    print("hello user ")
+else :
+    print("hello hacker ")    
